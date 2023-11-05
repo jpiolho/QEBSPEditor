6e568bb8f8bf89f54738438d04b4d7a1ed23fbf4
JPiolho <783467+jpiolho@users.noreply.github.com>
2023-11-05 09:42:17 +0100

    Improved texture loading speed dramatically by changing to BMPFormat and using stackalloc

---
+++ QEBSPEditor/Core/Utilities/BenchmarkScope.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+namespace QEBSPEditor.Core.Utilities
+{
+    public class BenchmarkScope : IDisposable
+    {
+        private Stopwatch _sw;
+        private string _name;
+        private BenchmarkScope? _parent;
+
+        public BenchmarkScope(string name)
+        {
+            _name = name;
+            _sw = Stopwatch.StartNew();
+        }
+
+        private BenchmarkScope(string name, BenchmarkScope parent) : this(name)
+        {
+            _parent = parent;
+        }
+
+        public void Dispose()
+        {
+            _sw.Stop();
+
+            Console.WriteLine($"{_name}: {_sw.Elapsed}");
+        }
+    }
+}
